a9095dfc04576daae2c2dc8a7a20ea6ea5dc6ec5
nicejwjin <jwjin@me.com>
2017-09-15 21:46:15 +0900

    - offline start 셋팅 추가

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+# add settings for each system.
+
+#gangreung setting
 export MONGO_URL=mongodb://localhost:27017/dasuploader
 export _CLIENT_NAME=gangReungSiCheong
-
-meteor -p4000
+meteor npm --save install oracle@0.3.9
+meteor npm install
+#meteor --release 1.3.5 -p4000
+METEOR_OFFLINE_CATALOG=1 meteor -p4000
