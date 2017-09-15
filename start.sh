d52d7e31d7c05ec2e6817685e3e4052b6e47bc0f
nicejwjin <jwjin@me.com>
2017-09-15 21:59:51 +0900

    - offline start 셋팅 추가

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ export _CLIENT_NAME=gangReungSiCheong
 meteor npm --save install oracle@0.3.9
 meteor npm install
 #meteor --release 1.3.5 -p4000
-METEOR_OFFLINE_CATALOG=1 meteor -p4000
+METEOR_OFFLINE_CATALOG=1 meteor -p4000  #METEOR 자체를 업데이트 하지 않도록 하는 환경 변수라는듯
