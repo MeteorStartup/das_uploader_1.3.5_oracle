04eebdfa337f3c12c7f78bfb69481afdfc1cbd7f
june <juner830310@gmail.com>
2017-11-10 14:26:33 +0900

    start.sh sample updated

--- start_sample.sh
+++ start_sample.sh
@@ -5,7 +5,9 @@
 #gangreung setting
 export MONGO_URL=mongodb://localhost:27017/dasuploader
 export _CLIENT_NAME=gangReungSiCheong
+export METEOR_OFFLINE_CATALOG=1   #don't update meteor itself
 meteor npm --save install oracle@0.3.9
 meteor npm install
+
 #meteor --release 1.3.5 -p4000
-METEOR_OFFLINE_CATALOG=1 meteor -p4000  #METEOR 자체를 업데이트 하지 않도록 하는 환경 변수라는듯
+meteor -p4000
